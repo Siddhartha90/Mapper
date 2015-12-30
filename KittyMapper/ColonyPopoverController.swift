58f2d183f99bc0aa1723185d84f014ba56432313
Siddhartha Gupta <siddhartha@hello.com>
2015-12-30 02:05:33 -0800

    Make a google pin appear on clicking, and a very crude popover.

---
+++ KittyMapper/ColonyPopoverController.swift
@@ -0,0 +1,17 @@
+//
+//  ColonyPopover.swift
+//  KittyMapper
+//
+//  Created by Siddhartha Gupta on 12/30/15.
+//
+//
+
+import UIKit
+
+class ColonyPopoverController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
--- KittyMapper/ViewController.swift
+++ KittyMapper/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 import GoogleMaps
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPopoverPresentationControllerDelegate {
     
     @IBOutlet weak var mapView: GMSMapView!
     
     let locationManager = CLLocationManager()
+    
 //    @IBOutlet weak var copyright: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         locationManager.delegate = self
+        mapView.delegate = self
         locationManager.requestWhenInUseAuthorization()
         
 //        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
@@ -29,11 +31,10 @@ class ViewController: UIViewController {
 //        self.view = mapView
 //        
 //        copyright.text = "KittyMapper©"
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
+    }
+    
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .None
     }
 
 }
@@ -45,6 +46,8 @@ extension ViewController: CLLocationManagerDelegate {
             locationManager.startUpdatingLocation()
             mapView.myLocationEnabled = true
             mapView.settings.myLocationButton = true
+            mapView.settings.indoorPicker = false;
+
          }
     }
 
@@ -54,5 +57,36 @@ extension ViewController: CLLocationManagerDelegate {
             locationManager.stopUpdatingLocation()
         }
     }
+}
+
+extension ViewController: GMSMapViewDelegate {
+    
+    func mapView(mapView: GMSMapView!, didTapAtCoordinate coordinate: CLLocationCoordinate2D) {
+        print("You tapped at \(coordinate.latitude), \(coordinate.longitude)")
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(coordinate.latitude, coordinate.longitude)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        
+//        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+//        UIViewController *controller = [storyboard instantiateViewControllerWithIdentifier:@"Pop"];
+        let vc = storyboard.instantiateViewControllerWithIdentifier("ColonyPopoverController") as! ColonyPopoverController
+        vc.modalPresentationStyle = .Popover
+        vc.preferredContentSize = CGSizeMake(200, 150)
+        
+        if let popoverController = vc.popoverPresentationController {
+            popoverController.delegate = self
+            popoverController.sourceRect = CGRectMake(100, 150, 20, 10)
+            popoverController.sourceView = self.view
+            self.presentViewController(vc, animated: true, completion: nil)
+        }
+        
+        
+        
+    }
+
 }
 
